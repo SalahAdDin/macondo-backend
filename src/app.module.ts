671fe584311050e8558154d58f012b997c625bd0
Luis Alaguna <alagunasalahaddin@live.com>
2021-01-29 19:21:10 +0300

    [BNF-4] - Adding an initial configuration for global app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
+import { GraphQLFederationModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DefaultAdminModule } from 'nestjs-admin';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 @Module({
-  imports: [],
+  imports: [
+    DefaultAdminModule,
+    GraphQLFederationModule.forRoot({
+      typePaths: ['**/*.graphql'],
+    }),
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        // transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+
+        transport: {
+          host: 'localhost',
+          port: 1025,
+          ignoreTLS: true,
+          secure: false,
+          auth: {
+            user: process.env.MAILDEV_INCOMING_USER,
+            pass: process.env.MAILDEV_INCOMING_PASS,
+          },
+        },
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>',
+        },
+        template: {
+          dir: process.cwd() + '/template/',
+          adapter: new EjsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

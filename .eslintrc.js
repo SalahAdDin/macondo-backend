64e0b3edcd91c08d2da424ccac8713f6a8480ba4
Josè Luis Sandoval Alaguna <alagunasalahaddin@live.com>
2021-02-03 19:00:52 +0000

    Merged in feature/luis/BNF-4-septup-nodejs-projects-with-full-l (pull request #1)
    [BNF-4] septup nodejs projects with full l
    
    * [BNF-4] - Installing required packages.
    * [BNF-4] - Adding configuration files.
    * [BNF-4] - Adding an initial configuration for global app module.
    * [BNF-4] - Installing packages for mondogb
    * [BNF-4] - Installing packages for mondogb
    * [BNF-4] - Installing fastify and apollo federation for graphql.
    * [BNF-4] - Fixing Apollo dependency.
    
    Approved-by: Programador Er
    Approved-by: Richar Pupo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,31 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+    project: "tsconfig.json",
+    sourceType: "module"
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ["@typescript-eslint/eslint-plugin", "import", "nestjs"],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+    "plugin:nestjs/recommended"
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [".eslintrc.js"],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off"
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import fastifyCookie from 'fastify-cookie';
+import fastifyCsrf from 'fastify-csrf';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.register(fastifyCookie, {
+    secret: 'my-secret', // TODO: generate it randomly(uuid) or set in environment file
+  });
+  await app.register(fastifyCsrf);
   await app.listen(3000);
 }
+
 bootstrap();
